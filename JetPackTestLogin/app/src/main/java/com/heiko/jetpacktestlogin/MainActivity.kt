82eaf38ce65c022b8dbbbb0938b175fd5df17cd6
Heiko <fenghenyu@163.com>
2019-12-12 09:12:24 +0800

    first commit

---
+++ JetPackTestLogin/app/src/main/java/com/heiko/jetpacktestlogin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.heiko.jetpacktestlogin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
